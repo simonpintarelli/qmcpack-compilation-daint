59ab889ac79c42f193d525ae06610b01cac3f598
Andrea Zen <andrea.zen@unina.it>
2021-04-16 11:55:56 +0200

    updated all builders, installed in build, gpu compilation

--- build_qmcpack-cpur-intel.sh
+++ build_qmcpack-cpur-intel.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-export mode=cpur
+export INSTALL_PREIFX=/apps/daint/SSL/simonpi/qmcpack-cpur
 export QMC_SOURCE_DIR=${HOME}/qmcpack-3.11.0
-bdir=${QMC_SOURCE_DIR}/build_$mode
 
-
+mkdir -p ${INSTALL_PREIFX}
 # load modules
 module swap PrgEnv-cray PrgEnv-gnu
 # module swap PrgEnv-cray PrgEnv-intel
 module load daint-gpu
 module load EasyBuild-custom/cscs
+module load cudatoolkit
 module load cray-hdf5-parallel
 module load CMake/3.14.5
 module load PyExtensions/python3-CrayGNU-20.11
@@ -24,7 +24,8 @@ module load intel
 # check what is loaded
 module list
 
-
+bdir=/scratch/snx3000/simonpi/qmcpack-cpur
+# build in /dev/shm
 (
     # clean build directory
     rm -rf ${bdir}
@@ -32,21 +33,17 @@ module list
     mkdir -p ${bdir}
     cd ${bdir}
     export CRAY_LINK_TYPE=dynamic
-    # qmcpack wants to set this, but it breaks FindCUDA.cmake:
-    # -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment
-    # install in default dir, into build
-    # -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-    CXX=CC cmake  \
-       -DENABLE_CUDA=Off \
+    CXX=CC cmake  -DQMC_CUDA=Off \
            -DQMC_MPI=On \
            -DQMC_OMP=On \
            -DQMC_COMPLEX=0 \
+           -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment \
            -DENABLE_PHDF5=On \
+           -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
            ${QMC_SOURCE_DIR}
     make -j8
     make install
 )
 
 # set permissions
-# export INSTALL_PREIFX=/apps/daint/SSL/simonpi/qmcpack-gpuc
-# find -type d /apps/daint/SSL/simonpi -exec chmod a+rx {} \;
+find -type d /apps/daint/SSL/simonpi -exec chmod a+rx {} \;
